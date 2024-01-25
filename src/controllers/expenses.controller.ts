61104e80cd0801f79645ec8c6e9e81d027be744d
Lucas Lorente <llorente@kovix-group.com>
2024-01-25 17:38:44 -0300

    expenses controller changes

--- src/controllers/expenses.controller.ts
+++ src/controllers/expenses.controller.ts
@@ -2,26 +2,26 @@ import { Request, Response } from "express";
 import prisma from "../prisma";
 
 class ExpensesController {
-    getAll = async (req: Request, res: Response) => {
-        try {
-            const expenses = await prisma.expenses.findMany();
-            res.json(expenses);
-          } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Error al obtener Gastos' });
-          }
-    };
+  getAll = async (req: Request, res: Response) => {
+    try {
+      const expenses = await prisma.expenses.findMany();
+      res.json(expenses);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener Gastos" });
+    }
+  };
 
-    create = async (req: Request, res: Response) => {
-      const { description, amount } = req.body
-      await prisma.expenses.create({
-        data:{
-          description,
-          amount,
-          date: new Date()
-        }
-      })
-    };
+  create = async (req: Request, res: Response) => {
+    const { description, amount, date } = req.body;
+    const expense = await prisma.expenses.create({
+      data: {
+        description,
+        amount,
+        date,
+      },
+    });
+    res.json(expense);
+  };
 }
 
 export default new ExpensesController();
