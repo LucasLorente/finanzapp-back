d9dcbb80b6c0fda5652b7b91fed96047b49d4c9c
Lucas Lorente <llorente@kovix-group.com>
2024-03-18 18:00:57 -0300

    weekly and total expenses

--- src/controllers/expenses.controller.ts
+++ src/controllers/expenses.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import prisma from "../prisma";
 
 class ExpensesController {
-  getAll = async (req: Request, res: Response) => {
+  getAll = async (_req: Request, res: Response) => {
     try {
       const expenses = await prisma.expenses.findMany();
       res.json(expenses);
@@ -23,6 +23,45 @@ class ExpensesController {
       },
     });
     res.json(expense);
+  };
+
+  getTotal = async (_req: Request, res: Response) => {
+    try {
+      const total = await prisma.expenses.aggregate({
+        _sum: {
+          amount: true,
+        },
+      });
+      res.json(total);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener Gastos" });
+    }
+  };
+
+  getWeeklyExpenses = async (_req: Request, res: Response) => {
+    try {
+      const currentDate = new Date();
+      const startOfWeek = new Date(currentDate);
+      startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay()); // Iniciar desde el primer día de la semana actual (domingo)
+      const endOfWeek = new Date(currentDate);
+      endOfWeek.setDate(endOfWeek.getDate() + (6 - endOfWeek.getDay())); // Terminar en el último día de la semana actual (sábado)
+
+      const weeklyExpenses = await prisma.expenses.aggregate({
+        _sum: {
+          amount: true,
+        },
+        where: {
+          date: {
+            gte: startOfWeek,
+            lte: endOfWeek,
+          },
+        },
+      });
+
+      res.json(weeklyExpenses);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener Gastos" });
+    }
   };
 }
 
--- src/routes/expenses.routes.ts
+++ src/routes/expenses.routes.ts
@@ -1,9 +1,11 @@
-import { Router } from 'express';
-import ExpensesController from '../controllers/expenses.controller';
+import { Router } from "express";
+import ExpensesController from "../controllers/expenses.controller";
 
 const router = Router();
 
 router.get("/", ExpensesController.getAll);
+router.get("/total", ExpensesController.getTotal);
+router.get("/weekly", ExpensesController.getWeeklyExpenses);
 router.post("/", ExpensesController.create);
 
 export default router;
